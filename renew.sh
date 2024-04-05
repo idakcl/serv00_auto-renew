b337d736520a26a00214f17fcf4173574ab6aa4b
Eric Lee <ericleedaily@gmail.com>
2024-04-06 07:25:11 +0800

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -20,19 +20,16 @@ EOF
 
 echo "已创建自动续订脚本 auto-renew.sh。"
 
-# 检查是否安装了 pm2
+# 检查是否安装了 pm2 并启动脚本
 if pm2 -v >/dev/null 2>&1; then
     echo "正在使用 pm2 启动脚本..."
-    chmod +x auto-renew.sh && pm2 start ./auto-renew.sh
+    chmod +x auto-renew.sh && pm2 start ./auto-renew.sh && pm2 save
+    echo "自动续订脚本已使用 pm2 启动，并已保存进程列表。"
 else
     echo "未检测到 pm2。请访问以下网址安装 pm2:"
     echo "https://github.com/giturass/cloudflared_freebsd/blob/main/install.sh"
 fi
 
 # 删除 renew.sh 文件
-if [ -f "renew.sh" ]; then
-    rm -f renew.sh
-    echo "本地 renew.sh 脚本已删除。"
-else
-    echo "未找到本地 renew.sh 脚本。"
-fi
+rm -f renew.sh
+echo "本地 renew.sh 脚本已删除。"
